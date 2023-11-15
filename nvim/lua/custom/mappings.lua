7eda628ca3ede71b694910fb21afdfba1c7bfd5f
UanBaito <fernandoanibalreyes@gmail.com>
2023-11-14 20:36:10 -0400

    update custom

--- nvim/lua/custom/mappings.lua
+++ nvim/lua/custom/mappings.lua
@@ -1,17 +1,1 @@
----@type MappingsTable
-local M = {}
-
-M.general = {
-  n = {
-    [";"] = { ":", "enter command mode", opts = { nowait = true } },
-    ["<leader>pp"] = {":Telescope registers <CR>", "Telescope registers"},
-    ["<leader>fh"] = {":Telescope find_files hidden=true no_ignore=true<CR>", "Find hidden files"}
-  },
-  v = {
-    [">"] = { ">gv", "indent"},
-  },
-}
-
--- more keybinds!
-
-return M
+/home/onebyte/.config/nvim/lua/custom/mappings.lua
